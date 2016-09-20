941246b865b70e3c95d9f1b80ecfc240c460f82b
vassmate <vassmate08@gmail.com>
2016-09-20 10:35:14 +0200

    Adding printing result

--- spiral/src/spiral/Spiral.java
+++ spiral/src/spiral/Spiral.java
@@ -8,10 +8,11 @@ public class Spiral {
 		}
 
 		int[][] spiralArray = new int[n][n];
-
+		int incr = 1;
 		for (int i = 0; i < n; i++) {
 			for (int j = 0; j < n; j++) {
-				spiralArray[i][j] = j;
+				spiralArray[i][j] = incr;
+				incr++;
 			}
 		}
 
@@ -19,6 +20,13 @@ public class Spiral {
 	}
 
 	public static void main(String[] args) {
+		int[][] sArray = createSpiral(5);
 
+		for (int[] is : sArray) {
+			for (int i = 0; i < is.length; i++) {
+				System.out.print(is[i] + ", ");
+			}
+			System.out.println(" ");
+		}
 	}
 }
