29b844181ce73bfbfdf5dc07829f7a6f8ec8aad6
vassmate <vassmate08@gmail.com>
2016-09-20 11:07:16 +0200

    Adding possible solution
    Not entirely my solution.
    
    Link for details:
    
    http://www.guideforschool.com/1260759-java-program-to-print-circular-spiral-matrix/

--- spiral/src/spiral/Spiral.java
+++ spiral/src/spiral/Spiral.java
@@ -7,26 +7,50 @@ public class Spiral {
 			return null;
 		}
 
-		int[][] spiralArray = new int[n][n];
-		int incr = 1;
-		for (int i = 0; i < n; i++) {
-			for (int j = 0; j < n; j++) {
-				spiralArray[i][j] = incr;
-				incr++;
+		int spiralArray[][] = new int[n][n];
+		int k = 1, colStartInd = 0, colEndInd = n - 1, rowStartInd = 0, rowEndInd = n - 1;
+
+		while (k <= n * n) {
+			for (int i = colStartInd; i <= colEndInd; i++) {
+				spiralArray[rowStartInd][i] = k++;
 			}
+
+			for (int j = rowStartInd + 1; j <= rowEndInd; j++) {
+				spiralArray[j][colEndInd] = k++;
+			}
+
+			for (int i = colEndInd - 1; i >= colStartInd; i--) {
+				spiralArray[rowEndInd][i] = k++;
+			}
+
+			for (int j = rowEndInd - 1; j >= rowStartInd + 1; j--) {
+				spiralArray[j][colStartInd] = k++;
+			}
+
+			colStartInd++;
+			colEndInd--;
+			rowStartInd++;
+			rowEndInd--;
 		}
 
 		return spiralArray;
 	}
 
 	public static void main(String[] args) {
-		int[][] sArray = createSpiral(5);
+
+		int n = 5;
+		int[][] sArray = createSpiral(n);
+
+		if (createSpiral(n) == null) {
+			System.out.println("Warning! The input number can't be less than 1!");
+			return;
+		}
 
 		for (int[] is : sArray) {
 			for (int i = 0; i < is.length; i++) {
-				System.out.print(is[i] + ", ");
+				System.out.print(is[i] + "  ");
 			}
-			System.out.println(" ");
+			System.out.println("");
 		}
 	}
 }
